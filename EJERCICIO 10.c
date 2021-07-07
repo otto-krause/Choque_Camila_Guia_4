70640a0651f85249ddcf1add06ff7e7c18dc55d5
CamilaChoque <86363351+CamilaChoque@users.noreply.github.com>
2021-07-07 11:19:18 -0300

    Add files via upload

---
+++ EJERCICIO 10.c
@@ -0,0 +1,45 @@
+#include <conio.h>
+#include <stdio.h>
+int main()
+{
+float sueld,SUELD1;
+printf("Ingrese su sueldo basico\n");
+scanf("%f",&sueld);
+printf("Ingrese a la categoria que pertenece\n");
+scanf("%d",&categ);
+if(categ==1)
+{
+ printf("Su sueldo basico es de $");
+ SUELD1=sueld+sueld/100*10;
+ printf("%.2f\n",SUELD1);
+}
+else
+{
+ if(categ==2)
+ {
+ printf("Su sueldo basico es de $");
+ SUELD1=sueld+sueld/100*20;
+ printf("%.2f\n",SUELD1);
+ }
+ else
+ {
+ printf("Su sueldo basico es de $");
+ printf("%.2f\n",sueld,"\n");
+}
+}
+ printf("\nIngresar la cantidad de hijos que tiene\n");
+ scanf("%d",&hijo);
+ if (hijo<=4)
+ {
+ PAGA=hijo*1000;
+ printf("La paga total de hijos es:$%d",PAGA);
+ }
+ else
+ {
+ PAGA=hijo*1500;
+ printf("La paga total de hijos es :$%d",PAGA);
+ }
+printf("\nLa paga mensual que recibira contando con todo lo anterior sera de:$");
+printf("%.2f\n",SUELD1+PAGA,"\n");
+return 0;
+}
---
+++ EJERCICIO 9.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ int hour,sueld;
+ char categ;
+ printf("\nIngrese la categoria a la que pertenece\n");
+ scanf("%c",&categ);
+ printf("Ingrese la cantidad de horas que trabaja\n");
+ scanf("%d",&hour);
+ if(categ=='A')
+ {
+ printf("Su sueldo ganado es:$%d",hour*200);
+ }
+ else
+ {
+ if(categ=='B')
+ {
+ printf("Su sueldo ganado es:$%d",hour*180);
+ }
+ else
+ {
+ if(categ=='C')
+ {
+ printf("Su sueldo ganado es:$%d",hour*150);
+ }
+ else
+ {
+ printf("Su sueldo ganado es:$0");
+ }
+ }
+ }
+ return 0;
+}
